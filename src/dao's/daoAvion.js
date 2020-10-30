a5a882d058b639ff6d06933d67774128d1b7080b
gaston <gaston.92@hotmail.com.ar>
2020-10-30 20:10:55 -0300

    Tercer commit con estructura de Dao's y servicios x c/u

---
+++ src/dao's/daoAvion.js
@@ -0,0 +1,29 @@
+const avion1 = { "modeloAvión": "BOEING 737 JET",
+"numeroAvion": "5312563643",   
+"capacidad":  "132"
+},
+const avion2 = { "modeloAvión": "BOEING 737 JET",
+"numeroAvion": "5312563643",   
+"capacidad":  "132"
+},
+
+const avion3 = { "modeloAvión": "BOEING 737 JET",
+"numeroAvion": "5312563643",   
+"capacidad":  "132"
+},
+
+const avion4 = { "modeloAvión": "BOEING 737 JET",
+"numeroAvion": "5312563643",   
+"capacidad":  "132"
+},
+
+const aviones=[]
+
+aviones.push(avion1)
+aviones.push(avion2)
+aviones.push(avion3)
+aviones.push(avion4)
+
+function getAvion(params){
+    return params
+}
---
+++ src/servicios/gas/pruebaGas.js
@@ -0,0 +1,58 @@
+impor
+
+const objeto = {
+    avion: {
+        "modeloAvión": "BOEING 737 JET",
+        "numeroAvion": "5312563643",
+        "capacidad": "132"
+    },
+    reserva: {
+        "id": "49284934",
+        "vuelo": "2938",
+        "destino": "NYC New York, United States",
+        "origen": "EZE Buenos Aires, Argentina",
+        "cantAsientos": "1",
+        "nroAsiento": "40H",
+        "clase": "economica"
+    },
+    pasajero: {
+        "nombre": "Pepe Carlos",
+        "apellido": "Argento",
+        "dni": "20724829",
+        "email": "pepeargento@gmail.com"
+    },
+    vuelo: {
+        "precio": "$120.382,00",
+        "duracion": "11:00",
+        "partida": "JUEVES 04 SEPTIEMBRE",
+        "arribo": "VIERNES 05 SEPTIEMBRE",
+        "horaPartida": "12:50 PM",
+        "horaArribo": "4:15 AM",
+        "millaje": "8.720 km"
+    },
+    destino: {
+        "nombre": "Nueva York, United States",
+        "aeropuerto": "JFK",
+        "terminal": "3"
+    },
+    origen: {
+        "nombre": "Buenos Aires, Argentina",
+        "aeropuerto": "EZE",
+        "terminal": "C"
+    }
+}
+
+venderVueloEtc(daoAvion,)
+
+const pasajero = dao1.getPasajero()
+const avion = dao2.getPasajero()
+const vuelo = dao3.getPasajero()
+
+const objetoGigante = [pasajero, avion, vuelo]
+
+return {
+
+    ventaBoleto() {
+
+    }
+}
