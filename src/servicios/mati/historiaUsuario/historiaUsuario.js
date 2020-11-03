6965079d3db9c577e7f39471312514b684fbe11d
Matías <matias@MBP-de-Matias.fibertel.com.ar>
2020-11-03 10:48:56 -0300

    'historia usuario'

--- src/servicios/mati/historiaUsuario/historiaUsuario.js
+++ src/servicios/mati/historiaUsuario/historiaUsuario.js
@@ -1,1 +1,8 @@
 console.log('historia usuario')
+/*Modificación de reserva en boleto de avión existente(Mati)
+-Validar que exista reserva 
+-Validar que exista cliente
+-Validar que exista vuelo 
+-Generar PDF con nuevo boleto
+-Enviar por mail
+-Cambiar alerta temporizada a la nueva fecha*/
