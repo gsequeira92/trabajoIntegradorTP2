85d89cb9d191a1201bf157e4344e7b2ac5f33a39
Matías <matias@MBP-de-Matias.fibertel.com.ar>
2020-11-03 19:42:19 -0300

    'se crea historia de usuario'

--- src/servicios/mati/historiaUsuario/historiaUsuario.js
+++ src/servicios/mati/historiaUsuario/historiaUsuario.js
@@ -6,3 +6,50 @@ console.log('historia usuario')
 -Generar PDF con nuevo boleto
 -Enviar por mail
 -Cambiar alerta temporizada a la nueva fecha*/
+
+const {crearMailer} = require('../leo/mailer/mailer') 
+const {crearTemporizador} = require('../gas/Temporizador')
+const {crearDaoCliente} = require('../daos/daoPasajeros')
+const {crearDaoReservas} = require('../daos/daoReservas')
+const {crearDaoVuelo} = require('../daos/daoVuelo')
+const { billeteVuelo } = require("./appBilleteVuelo");
+
+function modificarReservaVuelo() {
+
+    const tempo = crearTemporizador()
+    const mailer = crearMailer()
+
+    return {
+        execute: async (idCliente, idVuelo, idReserva) => {
+
+            esClienteValido(idCliente)
+            esVueloValido(idVuelo)
+            esReservaValida(idReserva)
+
+            const registroModificacion = modificarReserva(idReserva)
+            await daoReservas.cancelar(idReserva)
+
+            
+            const idNotificacion = tempo.crearNotificacionVuelo(idReserva)
+            tempo.cancelarEventoRecurrente(idNotificacion)
+            billeteVuelo(nombreArchivo, rutaArchivo, objeto) 
+            await mailer.enviarMail(idCliente)
+        }
+
+
+    }
+}
+
+async function  esClienteValido(idCliente){
+
+}
+
+async function esVueloValido(idVuelo){
+    
+}
+
+async function esReservaValida(idVuelo){
+    
+}
+
+module.exports = {modificarReservaVuelo}
