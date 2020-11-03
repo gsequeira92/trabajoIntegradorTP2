5ade3acba9f74e99253d680c435f809005790692
Matías <matias@MBP-de-Matias.fibertel.com.ar>
2020-11-03 10:32:58 -0300

    'agrego archivo appFacturaCancelacion actualizada'

--- src/servicios/mati/test/appFacturaCancelacionVuelo.js
+++ src/servicios/mati/test/appFacturaCancelacionVuelo.js
@@ -1,25 +1,23 @@
-//import PdfPrinter from "pdfmake"
 const PdfPrinter = require("pdfmake");
 const fs = require("fs");
-
-function facturaCancelada(nombreArchivo, rutaArchivo, objeto){
-
 const datosPDF = require('./datosParaPdf')
-datosPDF.recibirObjeto(objeto)
 const fonts = require("./fonts");
 const styles = require("./styles");
-const {content} = require("./pdfFacturaCancelacionVuelo");
 const { guardarArchivo } = require("./guardarArchivo.js");
+
+function facturaCancelada(nombreArchivo, rutaArchivo, objeto){
+datosPDF.verificarObjeto(objeto)
+const {content} = require("./pdfFacturaCancelacionVuelo");
 
 let docDefinition = {
     content: content,
     styles: styles
 };
-
 const printer = new PdfPrinter(fonts);
 let pdfDoc = printer.createPdfKitDocument(docDefinition);
 pdfDoc.pipe(guardarArchivo(rutaArchivo, nombreArchivo));
 pdfDoc.end();
+console.log('PDF guardado')
 
 }
 
