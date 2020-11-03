779bff667e83b9ceab8888e93a75105e7ec625c5
Matías <matias@MBP-de-Matias.fibertel.com.ar>
2020-11-03 10:33:55 -0300

    'agrego archivo appFactura actualizado'

--- src/servicios/mati/test/appFacturaVuelo.js
+++ src/servicios/mati/test/appFacturaVuelo.js
@@ -1,26 +1,23 @@
-//import PdfPrinter from "pdfmake"
 const PdfPrinter = require("pdfmake");
 const fs = require("fs");
-
-function factura(nombreArchivo, rutaArchivo, objeto){
-
 const datosPDF = require('./datosParaPdf')
-datosPDF.recibirObjeto(objeto)
-
 const fonts = require("./fonts");
 const styles = require("./styles");
-const {content} = require("./pdfFacturaVuelo");
 const { guardarArchivo } = require("./guardarArchivo.js");
+
+function factura(nombreArchivo, rutaArchivo, objeto){
+datosPDF.verificarObjeto(objeto)
+const {content} = require("./pdfFacturaVuelo");
 
 let docDefinition = {
     content: content,
     styles: styles
 };
-
 const printer = new PdfPrinter(fonts);
 let pdfDoc = printer.createPdfKitDocument(docDefinition);
 pdfDoc.pipe(guardarArchivo(rutaArchivo, nombreArchivo));
 pdfDoc.end();
+console.log('PDF guardado')
 
 }
 
