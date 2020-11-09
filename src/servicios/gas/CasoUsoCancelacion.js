b7f3d8ba79198b000211ca1ecef1293fe5386f40
gaston <gaston.92@hotmail.com.ar>
2020-11-09 17:03:57 -0300

    Modifico carpeta "gas" para el CU1 y 2

--- src/servicios/gas/CasoUsoCancelacion.js
+++ src/servicios/gas/CasoUsoCancelacion.js
@@ -1,67 +1,45 @@
-const { crearMailer } = require('../leo/mailer/mailer')
 const { crearTemporizador } = require('../gas/Temporizador')
+const { getMailer } = require('../factorys/factoryMailer.js')
 const { facturaCancelada } = require('../mati/test/appFacturaCancelacionVuelo')
 const { crearDaoCliente } = require('../daos/daoPasajeros')
 const { crearDaoReservas } = require('../daos/daoReservas')
 const rutaArchivo = '../mati/pdfs'
 
-const credencial = {}
-credencial.user = "exampletaller@outlook.com"
-credencial.pass = "leo12345"
-credencial.servicio = "outlook"
 
-const tempo = crearTemporizador()
-const mailer = crearMailer(credencial)
-const daoReservas = crearDaoReservas()
-//factory
-//dao por parametros o directo en la funcion?????
-function crearCUCancelacionReserva(mailer, tempo, facturaCancelada,daoReservas) {
+function crearCUCancelacionReserva({ mailer, tempo, facturaCancelada, daoReservas, daoClientes }) {
+
+    mailer = getMailer()
+    tempo = crearTemporizador()
+    pdfCancelacion = facturaCancelada()
+    daoReservas = crearDaoReservas()
+    daoClientes = crearDaoCliente()
 
     return {
         execute: async (idCliente, idReserva) => {
 
-            if (esClienteValido(idCliente)) {
+            const cliente = await daoClientes.getById(idCliente)
 
-                //Probablemente no haga falta
-                esReservaValida(idReserva)
-                const reserva = await obtenerReservaPorId(idReserva)
+            if (cliente) {
+                const reserva = await daoReservas.getReservaById(idReserva)
                 const mailPasajero = reserva.mail
 
                 //funcion que borra reserva de DB
-                cancelarReserva(idReserva, daoReservas)
+                await daoReservas.delete(idReserva)
 
                 //genera PDF con confirmacion de cancelacion
-                facturaCancelada(`${"cancelacion de reserva" + idReserva}`, rutaArchivo, reserva)
+                pdfCancelacion(`${"cancelacion de reserva" + idReserva}`, rutaArchivo, reserva)
 
-                //mailer envia mail
-                await mailer.enviarMail(mailPasajero, rutaArchivo)
+                //hay que agregar el pfd como adjunto al sobre del mailer
+                
+                 
+                //mailer deberia incluir el sobre para configurarlos de alguna forma y solo usar sendMail()
+                await mailer.sendMail(sobre)
                 tempo.cancelarEventoRecurrente(idReserva)
 
             }
 
         }
     }
-}
-
-async function obtenerReservaPorId(idReserva, daoReservas) {
-    //validar o lanzar excepcion
-    return await daoReservas.getById(idReserva)
-}
-
-async function cancelarReserva(idReserva, daoReservas) {
-    await daoReservas.delete(idReserva)
-}
-
-async function esClienteValido(idCliente) {
-
-    //query en base de datos para comprobar si existe cliente con ese id
-    //Devuelve boolean
-}
-
-async function esReservaValida(idVuelo) {
-
-    //query en base de datos para comprobar si existe reserva con ese id (y esta activa)
-    //Devuelve boolean
 }
 
 module.exports = { crearCUCancelacionReserva }
