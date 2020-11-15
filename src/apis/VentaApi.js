7fa509d48311fee02d3093f13285c67cfccfc006
Matías <matias@MacBook-Pro-de-Matias.local>
2020-11-15 13:07:59 -0300

    'modificacion en api de comida y api de venta'

--- src/apis/VentaApi.js
+++ src/apis/VentaApi.js
@@ -18,7 +18,3 @@ function crearVentaApi({crearBoleteriaAvion}){
 module.exports = {
     crearVentaApi
 }
-//creacions de apis
-//mejorar lo del router
-//conexion a mongo
-//casos de uso que usen daos de mongo
