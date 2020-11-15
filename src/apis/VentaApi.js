05d1a341a260ba4b341fa4071bb00badf6241fb9
Matías <matias@MacBook-Pro-de-Matias.local>
2020-11-15 12:57:21 -0300

    'carpeta para apis'

--- src/apis/VentaApi.js
+++ src/apis/VentaApi.js
@@ -18,3 +18,7 @@ function crearVentaApi({crearBoleteriaAvion}){
 module.exports = {
     crearVentaApi
 }
+//creacions de apis
+//mejorar lo del router
+//conexion a mongo
+//casos de uso que usen daos de mongo
