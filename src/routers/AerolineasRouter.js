17a38801572d9db3ce764d11c530aeb89e8e1377
Matías <matias@MacBook-Pro-de-Matias.local>
2020-11-15 15:06:58 -0300

    'modificacion en routers, se unifica'

--- src/routers/AerolineasRouter.js
+++ src/routers/AerolineasRouter.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const aerolineaApi = require('../apis/AerolineaApi')
 
 //tiene que recibir la api por parametro y asignarla despues a una constante para usar
 function aerolineasRouter({ aerolineaApi }) {
@@ -8,19 +7,36 @@ function aerolineasRouter({ aerolineaApi }) {
     //esta api se va a encargar de borrar la reserva del DAO (apiAerolineas.deleteById(id))
     const apiAerolineas = aerolineaApi
 
+    router.post('/ventas', async (req, res) => {
+
+        const reserva = await apiAerolineas.venderPasaje(req.body)
+        res.status(201).json(reserva)
+
+    })
+
+    router.put('/reservas/::id&true', async (req, res) => {
+        try {
+            const algo = await apiModificacion.modificarComida(req.params)
+            res.status(204).json()
+        } catch (e) {
+            res.status(e.getMessage).json()
+        }
+    })
+
     router.delete('/reservas/:id', async (req, res) => {
 
         try {
             await apiAerolineas.cancelarReserva(req.params.id)
             res.status(204).json()
         } catch (error) {
-            const mensajeError =  res.statusMessage()
+            const mensajeError = res.statusMessage()
             console.log(mensajeError, error.getMessage())
             res.status(res.statusCode).json()
         }
 
     })
 
+    return router
 }
 
 module.exports = { aerolineasRouter }
---
+++ src/servicios/routersViejos/CancelacionRouter.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const aerolineaApi = require('../../apis/AerolineaApi')
+
+//tiene que recibir la api por parametro y asignarla despues a una constante para usar
+function aerolineasRouter({ aerolineaApi }) {
+
+    const router = express.Router()
+    //esta api se va a encargar de borrar la reserva del DAO (apiAerolineas.deleteById(id))
+    const apiAerolineas = aerolineaApi
+
+    router.delete('/reservas/:id', async (req, res) => {
+
+        try {
+            await apiAerolineas.cancelarReserva(req.params.id)
+            res.status(204).json()
+        } catch (error) {
+            const mensajeError =  res.statusMessage()
+            console.log(mensajeError, error.getMessage())
+            res.status(res.statusCode).json()
+        }
+
+    })
+
+}
+
+module.exports = { aerolineasRouter }
